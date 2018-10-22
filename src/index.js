c4270dcb233797758b3415c80bbf1aa07fbc2928
Luis <luisdiazvenero@gmail.com>
2018-10-22 12:39:58 -0500

    instalando y configurando dependencias y reducers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import * as reducers from '.reducers'
+import {reducer as formReducer} from 'redux-form'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers({
+  noticias: noticiasReducer,
+  categorias: categoriasReducer,
+  form: formReducer
+}))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />, document.getElementById('root'));
+  </Provider>
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
